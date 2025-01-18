0720fc22ae6ba92799921c6c2272ed3f70077b3f
David H <davyd2h@gmail.com>
2025-01-17 21:04:38 -0500

    se crea estructura de mi proyecto, fronted ok, desarrollando el backend

--- formDinámicoRegistroDatos/app.js
+++ formDinámicoRegistroDatos/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const path = require('path')
+const apiRoutes = require('./routes/api')
 
 const app = express()
 const port = 5000;
 
+// Middlewares
 app.use(cors())
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server is running on port  http://localhost:${port}`)
+})
+
+app.use('/api', apiRoutes) // api routes
