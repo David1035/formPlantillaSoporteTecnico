be0fd77f15fa707bfe141dff63a33051998e2d44
David H <davyd2h@gmail.com>
2025-01-20 14:20:45 -0500

    plantilla rescate virtual, se crea fronted para consultar todos los registros y modificar

--- controllers/formController.js
+++ controllers/formController.js
@@ -3,7 +3,9 @@ const Form = require('../models/Form')
 //Obtener todos los registros
 const getAllForm = async (req, res) => {
     try {
-        const forms = await Form.findAll();
+        const forms = await Form.findAll({
+            order: [['id', 'DESC']] // Ordenar de manera descendente
+        });
         res.json(forms)
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -13,7 +15,6 @@ const getAllForm = async (req, res) => {
 // crear un nuevo registtro
 const createForm = async (req, res) => {
     try {
-        console.log('Datos recibidos:', req.body);
         const form = await Form.create(req.body);
         res.status(201).json(form)
     } catch (error) {
@@ -57,5 +58,29 @@ const deleteForm = async (req, res) => {
     }
 }
 
-module.exports = { getAllForm, createForm, getFormById, updateForm, deleteForm }
+const searchDocumentoForm = async (req, res) => {
+    try {
+        const { documentoIdentidad } = req.params;
+        if(!documentoIdentidad) return res.status(400).json({ message: 'Documento requerido' })
+        const form = await Form.findAll({ where: { documentoIdentidad } });
+        if(!form.length === 0) return res.status(404).json({ message: 'No se encontraron resultados' })
+        res.json(form)
+    } catch (error) {
+        res.status(500).json({ message: 'Documento no encontradol' })
+    }
+}
+
+const searchIdLlamadaForm = async (req, res) => {
+    try {
+        const { idLlamada } = req.params;
+        if(!idLlamada) return res.status(400).json({ message: 'Idllamada es requerido' })
+        const form = await Form.findOne( { where: { idLlamada}})
+        if(!form.length === 0) return res.status(404).json({ message: 'Id llamada no encontrado'})
+        res.json(form)
+    } catch (error) {
+        res.status(500).json({ message: 'id no encontrado en mi bd'} )
+    }
+}
+
+module.exports = { getAllForm, createForm, getFormById, updateForm, deleteForm, searchDocumentoForm, searchIdLlamadaForm }
 
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllForm, createForm, getFormById, updateForm, deleteForm } = require('../controllers/formController');
+const { getAllForm, createForm, getFormById, updateForm, deleteForm, searchDocumentoForm, searchIdLlamadaForm } = require('../controllers/formController');
 
 router.get('/forms', getAllForm)
 router.post('/forms', createForm)
 router.get('/forms/:id', getFormById)
 router.put('/forms/:id', updateForm)
 router.delete('/forms/:id', deleteForm)
+router.get('/forms/documento/:documentoIdentidad', searchDocumentoForm)
+router.get('/forms/llamada/:idLlamada', searchIdLlamadaForm)
 
 module.exports = router;
