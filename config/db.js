e8e4bb55e6a5987c0f7ca6b9ea46953e528c37f1
David H <davyd2h@gmail.com>
2025-01-20 15:09:25 -0500

    se borra plantillas ejecutables, se corrige db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: path.resolve(__dirname, 'db/database.sqlite')
+    storage: 'db/database.sqlite'
 })
 
 module.exports = sequelize;  // Export the Sequelize instance
